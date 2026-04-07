dba695b78862c65a311ac8f18b3c2282d0a09a47
Mohammed Danish <danishmohammed819@gmail.com>
2026-04-08 02:59:21 +0530

    Changes Done

--- frontend/script.js
+++ frontend/script.js
@@ -30,7 +30,7 @@ async function sendMessage() {
   addMessage(text, "user");
   input.value = "";
 
-  const res = await fetch("http://localhost:3000/chat", {
+  const res = await fetch("https://premiumnonsense-production.up.railway.app/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -44,7 +44,7 @@ async function sendMessage() {
 
   // meme
   if (Math.random() < 0.3) {
-    const memeRes = await fetch("http://localhost:3000/meme");
+    const memeRes = await fetch("https://premiumnonsense-production.up.railway.app/meme");
     const meme = await memeRes.json();
     addImage(meme.url);
   }
